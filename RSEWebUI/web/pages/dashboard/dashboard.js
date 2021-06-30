b36fe03870274f1783e1fc357a3d97005e0c41cb
Elinoy Amar <amarelinoy@gmail.com>
2021-06-30 15:22:56 +0300

    continue to edit the dashboard window (including html and css)

--- RSEWebUI/web/pages/dashboard/dashboard.js
+++ RSEWebUI/web/pages/dashboard/dashboard.js
@@ -5,12 +5,23 @@ function loadXMLFile() {
         // you can use this method to get file and perform respective operations
         let files =   Array.from(input.files);
         console.log(files);
+        // todo: add here the call to the loading xml servlet
     };
     input.click();
 
 }
 
-function stockSelected(stockSymbol){
+function stockSelected(r,rowId){
+/*var symbol = document.getElementById(rowId).getElementsByTagName(dt)[0].value();
+$(function ())*/
 
+    let symbol = this.getElementsByTagName('dt')[0].value(); // todo: check if this works properly
+    $.ajax({
+        type:'get',
+        data:symbol,
+        url: "enter here",                                  // todo: replace the url to the real servlet
+        success: function(result){
+            window.location.reload(result);
+        }});
 
 }
