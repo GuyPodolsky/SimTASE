b1d76acf9c0f192da0c89327c9981bc20000bad7
Elinoy Amar <amarelinoy@gmail.com>
2021-07-08 16:35:01 +0300

    Added the buy and sell commands in page 3. - can't check if it works properly because the add trade commands is not working.  Fixed bug in showing existing stock that a user don't have shares of.

--- RSEWebUI/web/pages/stockview/stockview.js
+++ RSEWebUI/web/pages/stockview/stockview.js
@@ -6,6 +6,8 @@ $(function () { // onload
 
 
 })
+
+
 function getCookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
@@ -21,6 +23,7 @@ function getCookie(cname) {
     }
     return "";
 }
+
 function updateShowStock(){
     let stockSymbol = getCookie("stock");
     $.ajax({
@@ -35,18 +38,73 @@ function updateShowStock(){
             let username = data["username"];
             let userHoldings = data["userHoldings"];
             let stock = data["stock"];
-            document.getElementById("username-greet").innerText ="Hi "+username;
-            document.getElementById("company-name").innerHTML ="Company Name: " + stock["companyName"];
-            document.getElementById("share-value").innerHTML ="Share Value: " + stock["sharePrice"];
-            document.getElementById("turnover").innerHTML ="Turnover: " + stock["transactionsTurnOver"];
-            document.getElementById("own-holdings").innerHTML ="You own " + userHoldings +" shares";
-            document.getElementById("details-box-header").children[0].innerHTML =stock["symbol"] +" Stock View";
+            document.getElementById("username-greet").innerText = "Hi " + username;
+            document.getElementById("company-name").innerHTML = "Company Name: " + stock["companyName"];
+            document.getElementById("share-value").innerHTML = "Share Value: " + stock["sharePrice"];
+            document.getElementById("turnover").innerHTML = "Turnover: " + stock["transactionsTurnOver"];
+            document.getElementById("own-holdings").innerHTML = "You own " + userHoldings + " shares";
+            document.getElementById("details-box-header").children[0].innerHTML = stock["symbol"] + " Stock View";
 
-            for(var i =0;i<stock["stockTransactions"].length;i++){ // to show the stock transactions
-                let tran = stock["stockTransactions"][i];   // the i'th transaction
+            // Show the stock transactions
+            let transBody = document.getElementById("transactions-table-body");
+            let transactions = stock["stockTransactions"];
+            transBody.innerHTML = "";
+            for (var i = 0; i < transactions.length; i++) {
+                let tran = transactions[i];   // the i'th transaction
+                let row = transBody.insertRow(0);
+                let cell1 = row.insertCell(0);
+                let cell2 = row.insertCell(1);
+                let cell3 = row.insertCell(2);
+                let cell4 = row.insertCell(3);
+                let cell5 = row.insertCell(4);
+                cell1.innerHTML = tran["formattedTimestamp"];
+                cell2.innerHTML = tran["quantity"];
+                cell3.innerHTML = tran["price"];
+                cell4.innerHTML = tran["seller"]["username"];
+                cell5.innerHTML = tran["buyer"]["username"];
+            } // for
 
-            }
-        }
+            // Show the stock awaiting buy trade commands
+            let buycommandsBody = document.getElementById("buy-commands-table-body");
+            let buyCommands = stock["buyCommandDTs"];
+            buycommandsBody.innerHTML = "";
+            for (var i = 0; i < buyCommands.length; i++) {
+                let buy = buyCommands[i];   // the i'th transaction
+                let row = buycommandsBody.insertRow(0);
+                let cell1 = row.insertCell(0);
+                let cell2 = row.insertCell(1);
+                let cell3 = row.insertCell(2);
+                let cell4 = row.insertCell(3);
+                let cell5 = row.insertCell(4);
+                let cell6 = row.insertCell(5);
+                cell1.innerHTML = buy["formattedDateTime"];
+                cell2.innerHTML = buy["user"]["username"];
+                cell3.innerHTML = "Buy command";
+                cell4.innerHTML = buy["commandType"];
+                cell5.innerHTML = buy["wantedPrice"];
+                cell6.innerHTML = buy["quantity"];
+            } // for
 
-    })
+            // Show the stock awaiting sell trade commands
+            let sellcommandsBody = document.getElementById("sell-commands-table-body");
+            let sellCommands = stock["sellCommandDTs"];
+            sellcommandsBody.innerHTML = "";
+            for (var i = 0; i < sellCommands.length; i++) {
+                let sell = sellCommands[i];   // the i'th transaction
+                let row = sellcommandsBody.insertRow(0);
+                let cell1 = row.insertCell(0);
+                let cell2 = row.insertCell(1);
+                let cell3 = row.insertCell(2);
+                let cell4 = row.insertCell(3);
+                let cell5 = row.insertCell(4);
+                let cell6 = row.insertCell(5);
+                cell1.innerHTML = sell["formattedDateTime"];
+                cell2.innerHTML = sell["user"]["username"];
+                cell3.innerHTML = "Sell command";
+                cell4.innerHTML = sell["commandType"];
+                cell5.innerHTML = sell["wantedPrice"];
+                cell6.innerHTML = sell["quantity"];
+            } // for
+        } // successes
+    })// ajax call
 }
